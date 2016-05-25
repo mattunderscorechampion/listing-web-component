fe48040e12563fce9e076cf5576b1a1b84c2c8c4
Matt Champion <mattunderscorechampion@gmail.com>
2016-05-25 21:33:27 +0100

    Add loading placeholder to standalone code.

--- src/code-block.js
+++ src/code-block.js
@@ -10,6 +10,12 @@
         var dataSrc = this.getAttribute('data-src');
         var gistElement = this;
 
+        var shadow = gistElement.createShadowRoot();
+        var loading = document.createElement('pre');
+        loading.style = 'text-align: left; font-size: 10pt; border: 1px solid grey';
+        loading.textContent = 'Loading';
+        shadow.appendChild(loading);
+
         var request = new XMLHttpRequest();
         request.onreadystatechange = function () {
             var httpResponse = this;
@@ -17,14 +23,19 @@
             if (httpResponse.readyState === DONE) {
                 var shadow = gistElement.createShadowRoot();
                 var style = document.createElement('style');
-                style.innerHTML = '.hljs-comment,.hljs-quote{color:#8e908c}.hljs-variable,.hljs-template-variable,.hljs-tag,.hljs-name,.hljs-selector-id,.hljs-selector-class,.hljs-regexp,.hljs-deletion{color:#c82829}.hljs-number,.hljs-built_in,.hljs-builtin-name,.hljs-literal,.hljs-type,.hljs-params,.hljs-meta,.hljs-link{color:#f5871f}.hljs-attribute{color:#eab700}.hljs-string,.hljs-symbol,.hljs-bullet,.hljs-addition{color:#718c00}.hljs-title,.hljs-section{color:#4271ae}.hljs-keyword,.hljs-selector-tag{color:#8959a8}.hljs{display:block;overflow-x:auto;background:white;color:#4d4d4c;padding:0.5em}.hljs-emphasis{font-style:italic}.hljs-strong{font-weight:bold}';
+                style.textContent = '.hljs-comment,.hljs-quote{color:#8e908c}.hljs-variable,.hljs-template-variable,.hljs-tag,.hljs-name,.hljs-selector-id,.hljs-selector-class,.hljs-regexp,.hljs-deletion{color:#c82829}.hljs-number,.hljs-built_in,.hljs-builtin-name,.hljs-literal,.hljs-type,.hljs-params,.hljs-meta,.hljs-link{color:#f5871f}.hljs-attribute{color:#eab700}.hljs-string,.hljs-symbol,.hljs-bullet,.hljs-addition{color:#718c00}.hljs-title,.hljs-section{color:#4271ae}.hljs-keyword,.hljs-selector-tag{color:#8959a8}.hljs{display:block;overflow-x:auto;background:white;color:#4d4d4c;padding:0.5em}.hljs-emphasis{font-style:italic}.hljs-strong{font-weight:bold}';
                 shadow.appendChild(style);
                 var pre = document.createElement('pre');
                 pre.style = 'text-align: left; font-size: 10pt; border: 1px solid grey';
                 var code = document.createElement('code');
-                code.innerHTML = httpResponse.responseText;
-                pre.appendChild(code);
+                code.textContent = httpResponse.responseText;
                 hljs.highlightBlock(code);
+                pre.appendChild(code);
+
+                while (shadow.firstChild) {
+                    shadow.removeChild(shadow.firstChild);
+                }
+
                 shadow.appendChild(pre);
             }
         };
