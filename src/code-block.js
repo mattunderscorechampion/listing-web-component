ba5cfd3de6a1ce229c2aadcff1bc58f93b47001a
Matt Champion <mattunderscorechampion@gmail.com>
2016-05-25 21:46:08 +0100

    Do not create two shadow roots.

--- src/code-block.js
+++ src/code-block.js
@@ -21,7 +21,6 @@
             var httpResponse = this;
             var DONE = httpResponse.DONE || 4;
             if (httpResponse.readyState === DONE) {
-                var shadow = gistElement.createShadowRoot();
                 var style = document.createElement('style');
                 style.textContent = '.hljs-comment,.hljs-quote{color:#8e908c}.hljs-variable,.hljs-template-variable,.hljs-tag,.hljs-name,.hljs-selector-id,.hljs-selector-class,.hljs-regexp,.hljs-deletion{color:#c82829}.hljs-number,.hljs-built_in,.hljs-builtin-name,.hljs-literal,.hljs-type,.hljs-params,.hljs-meta,.hljs-link{color:#f5871f}.hljs-attribute{color:#eab700}.hljs-string,.hljs-symbol,.hljs-bullet,.hljs-addition{color:#718c00}.hljs-title,.hljs-section{color:#4271ae}.hljs-keyword,.hljs-selector-tag{color:#8959a8}.hljs{display:block;overflow-x:auto;background:white;color:#4d4d4c;padding:0.5em}.hljs-emphasis{font-style:italic}.hljs-strong{font-weight:bold}';
                 shadow.appendChild(style);
