d2b873fe9de4d106081dd19b318ed3ccc03c8d4c
Matt Champion <mattunderscorechampion@gmail.com>
2016-05-28 21:50:11 +0100

    Allow text to be inlined in the custom element.

--- src/code-block.js
+++ src/code-block.js
@@ -12,8 +12,15 @@
 
         var shadow = gistElement.createShadowRoot();
         var loading = document.createElement('pre');
+        var loadingCode = document.createElement('code');
+        loading.appendChild(loadingCode);
         loading.style = 'text-align: left; font-size: 10pt; border: 1px solid grey';
-        loading.textContent = 'Loading';
+        if (gistElement.textContent && gistElement.textContent.trim()) {
+            loadingCode.textContent = gistElement.textContent.trim();
+            hljs.highlightBlock(loadingCode);
+        } else {
+            loadingCode.textContent = 'Loading';
+        }
         if (dataCaption) {
             var loadingCaption = document.createElement('div');
             loadingCaption.style = 'border-top: 1px solid grey; text-align: center;';
@@ -22,36 +29,38 @@
         }
         shadow.appendChild(loading);
 
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function () {
-            var httpResponse = this;
-            var DONE = httpResponse.DONE || 4;
-            if (httpResponse.readyState === DONE) {
-                var style = document.createElement('style');
-                style.textContent = '.hljs-comment,.hljs-quote{color:#8e908c}.hljs-variable,.hljs-template-variable,.hljs-tag,.hljs-name,.hljs-selector-id,.hljs-selector-class,.hljs-regexp,.hljs-deletion{color:#c82829}.hljs-number,.hljs-built_in,.hljs-builtin-name,.hljs-literal,.hljs-type,.hljs-params,.hljs-meta,.hljs-link{color:#f5871f}.hljs-attribute{color:#eab700}.hljs-string,.hljs-symbol,.hljs-bullet,.hljs-addition{color:#718c00}.hljs-title,.hljs-section{color:#4271ae}.hljs-keyword,.hljs-selector-tag{color:#8959a8}.hljs{display:block;overflow-x:auto;background:white;color:#4d4d4c;padding:0.5em}.hljs-emphasis{font-style:italic}.hljs-strong{font-weight:bold}';
-                shadow.appendChild(style);
-                var pre = document.createElement('pre');
-                pre.style = 'text-align: left; font-size: 10pt; border: 1px solid grey';
-                var code = document.createElement('code');
-                code.textContent = httpResponse.responseText;
-                hljs.highlightBlock(code);
-                pre.appendChild(code);
-                if (dataCaption) {
-                    var caption = document.createElement('div');
-                    caption.style = 'border-top: 1px solid grey; text-align: center;';
-                    caption.textContent = dataCaption;
-                    pre.appendChild(caption);
-                }
+        if (dataSrc) {
+            var request = new XMLHttpRequest();
+            request.onreadystatechange = function () {
+                var httpResponse = this;
+                var DONE = httpResponse.DONE || 4;
+                if (httpResponse.readyState === DONE) {
+                    var style = document.createElement('style');
+                    style.textContent = '.hljs-comment,.hljs-quote{color:#8e908c}.hljs-variable,.hljs-template-variable,.hljs-tag,.hljs-name,.hljs-selector-id,.hljs-selector-class,.hljs-regexp,.hljs-deletion{color:#c82829}.hljs-number,.hljs-built_in,.hljs-builtin-name,.hljs-literal,.hljs-type,.hljs-params,.hljs-meta,.hljs-link{color:#f5871f}.hljs-attribute{color:#eab700}.hljs-string,.hljs-symbol,.hljs-bullet,.hljs-addition{color:#718c00}.hljs-title,.hljs-section{color:#4271ae}.hljs-keyword,.hljs-selector-tag{color:#8959a8}.hljs{display:block;overflow-x:auto;background:white;color:#4d4d4c;padding:0.5em}.hljs-emphasis{font-style:italic}.hljs-strong{font-weight:bold}';
+                    shadow.appendChild(style);
+                    var pre = document.createElement('pre');
+                    pre.style = 'text-align: left; font-size: 10pt; border: 1px solid grey';
+                    var code = document.createElement('code');
+                    code.textContent = httpResponse.responseText;
+                    hljs.highlightBlock(code);
+                    pre.appendChild(code);
+                    if (dataCaption) {
+                        var caption = document.createElement('div');
+                        caption.style = 'border-top: 1px solid grey; text-align: center;';
+                        caption.textContent = dataCaption;
+                        pre.appendChild(caption);
+                    }
 
-                while (shadow.firstChild) {
-                    shadow.removeChild(shadow.firstChild);
+                    while (shadow.firstChild) {
+                        shadow.removeChild(shadow.firstChild);
+                    }
+                    shadow.appendChild(pre);
                 }
-                shadow.appendChild(pre);
-            }
-        };
+            };
 
-        request.open('GET', dataSrc, true);
-        request.send(null);
+            request.open('GET', dataSrc, true);
+            request.send(null);
+        }
     };
 
     document.registerElement('x-code-block', {
