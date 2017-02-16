c7bff2d6daac0966ccffe85820c2bcb32919d54c
Matt Champion <mattunderscorechampion@gmail.com>
2017-02-16 21:34:44 +0000

    Parameterise the HTML import build highlight.js version as well as the style.

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,13 @@ var gulp = require('gulp'),
     filter = require('gulp-filter');
 
 gulp.task('generate', function() {
+    var highlightVersion = '9.4.0';
+    var hightlightStyle = 'tomorrow';
+    var hightlightJsUrl = '//cdn.jsdelivr.net/highlight.js/' + highlightVersion + '/highlight.min.js';
+    var hightlightStyleUrl = '//cdnjs.cloudflare.com/ajax/libs/highlight.js/' + highlightVersion + '/styles/' + hightlightStyle + '.min.css';
+
     return gulp.src('src/templates/*')
-        .pipe(nunjucks.compile({style : '//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.4.0/styles/zenburn.min.css'}))
+        .pipe(nunjucks.compile({highlightJS : hightlightJsUrl, highlightCSS : hightlightStyleUrl}))
         .pipe(filter(['**/listing-import.html', '**/listing-script.js']))
         .pipe(gulp.dest('target/dist'))
 });
