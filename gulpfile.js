3a429501f42ad86b5dfb16f0116b255a4754d129
Matt Champion <mattunderscorechampion@gmail.com>
2017-02-16 22:25:59 +0000

    Load the parameteried style and inline it into the script during the build.

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,54 @@
 
 var gulp = require('gulp'),
     nunjucks = require('gulp-nunjucks'),
-    filter = require('gulp-filter');
+    filter = require('gulp-filter'),
+    http = require('http');
 
-gulp.task('generate', function() {
-    var highlightVersion = '9.9.0';
-    var hightlightStyle = 'tomorrow';
+var highlightVersion = '9.9.0';
+var hightlightStyle = 'tomorrow';
+var style;
+
+function loadStyle(version, style) {
+    return new Promise(function (accept, reject) {
+        if (version == undefined || style == undefined || version == null || style == null) {
+            reject(new Error('Two parameters needed'));
+            return;
+        }
+
+        http.get({
+            hostname: 'cdnjs.cloudflare.com',
+            port: 80,
+            path: '/ajax/libs/highlight.js/' + version + '/styles/' + style + '.min.css'
+        }, function (res) {
+            if (res.statusCode != 200) {
+                reject(new Error('Status code ' + res.statusCode));
+            }
+
+            var data = '';
+            res.on('data', function (chunk) {
+                data += chunk;
+            });
+            res.on('end', function () {
+                accept(data);
+            });
+        }).on('error', function (e) {
+            reject(e);
+        });
+    });
+}
+
+gulp.task('load-style', function () {
+    return loadStyle(highlightVersion, hightlightStyle).then(function (s) {
+        style = s;
+    });
+});
+
+gulp.task('generate', ['load-style'], function() {
     var hightlightJsUrl = '//cdn.jsdelivr.net/highlight.js/' + highlightVersion + '/highlight.min.js';
     var hightlightStyleUrl = '//cdnjs.cloudflare.com/ajax/libs/highlight.js/' + highlightVersion + '/styles/' + hightlightStyle + '.min.css';
 
     return gulp.src('src/templates/*')
-        .pipe(nunjucks.compile({highlightJS : hightlightJsUrl, highlightCSS : hightlightStyleUrl}))
+        .pipe(nunjucks.compile({highlightJS : hightlightJsUrl, highlightCSS : hightlightStyleUrl, inlineStyle: style}))
         .pipe(filter(['**/listing-import.html', '**/listing-script.js']))
         .pipe(gulp.dest('target/dist'))
 });
